a00adf71bed898d252921cc949df3f5145078f27
Mangostar <influencia.x.94@gmail.com>
2021-12-08 00:34:00 -0300

    Agregando codigo inicial de la calculadora en la web

---
+++ js/Calculadora-js.js
@@ -0,0 +1,31 @@
+const nueve = document.getElementById("9").onclick;
+
+const ocho = document.getElementById("8").onclick;
+
+const siete = document.getElementById("7").onclick;
+
+const seis = document.getElementById("6").onclick;
+
+const cinco = document.getElementById("5").onclick;
+
+const cuatro = document.getElementById("4").onclick;
+
+const tres = document.getElementById("3").onclick;
+
+const dos = document.getElementById("2").onclick;
+
+const uno = document.getElementById("1").onclick;
+
+const cero = document.getElementById("0").onclick;
+
+const suma = document.getElementById("suma").onclick;
+
+const resta = document.getElementById("resta").onclick;
+
+const divicion = document.getElementById("divicion").onclick;
+
+const multipli = document.getElementById("multiplicacion").onclick;
+
+const igual = document.getElementById("igual").onclick;
+
+let resultado = document.getElementById("resultados");
