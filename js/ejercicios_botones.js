ed32eab93bd5ad8565444b5f3edfeb488dc5d376
Mangostar <influencia.x.94@gmail.com>
2021-12-11 19:04:31 -0300

    Hice funcionar los radio buttons con los elementos, ahora hace cada boton muestra un elemento y desaparece el otro elemento si ya esta invocado

--- js/ejercicios_botones.js
+++ js/ejercicios_botones.js
@@ -1,12 +1,12 @@
 var botonUno = document.getElementById("button_1");
 var botonDos = document.getElementById("button_2");
 
-botonUno.onclick = function One(clicked = false) {    
-    clicked = true;    
+botonUno.onclick = function One() {    
     document.getElementById("calcula").style = "display: flex";
+    document.getElementById("calorias").style = "display: none";
 }
 
-botonDos.onclick = function Two(clicked = false) {    
-    clicked = true;    
-    document.getElementById("item_2").style = "display: flex";
+botonDos.onclick = function Two() {
+    document.getElementById("calcula").style = "display: none";
+    document.getElementById("calorias").style = "display: flex";
 }
