fd801c264fd3cc4a763c3345d1a855625c66394f
Omar Zavala <influencia.x.94@gmail.com>
2022-02-10 00:02:16 -0300

    actualizacion de estilos en nav

--- js/calorias.js
+++ js/calorias.js
@@ -1,13 +1,13 @@
 const envia = document.getElementById("enviar");
-var respuesta = document.getElementById("resulta");
+let respuesta = document.getElementById("resulta");
 
 envia.onclick = function calculo() {
-    var peso = document.getElementById("peso").value;
-    var altura = document.getElementById("altura").value;
-    var edad = document.getElementById("edad").value;
-    var genero = document.getElementById("genero");
-    var hombre = 66+(13.7*peso)+(5*altura)-(6.5*edad);
-    var mujer = 655+(9.6*peso)+(1.8*altura)-(4.7*edad);
+    let peso = document.getElementById("peso").value;
+    let altura = document.getElementById("altura").value;
+    let edad = document.getElementById("edad").value;
+    let genero = document.getElementById("genero");
+    let hombre = 66+(13.7*peso)+(5*altura)-(6.5*edad);
+    let mujer = 655+(9.6*peso)+(1.8*altura)-(4.7*edad);
 
     if (genero.value == 1) {
 
