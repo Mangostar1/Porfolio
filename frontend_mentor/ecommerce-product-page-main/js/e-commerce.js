97a607c1e4c53dde01aecbaf7d3f38ab46aa94d4
Omar Zavala <influencia.x.94@gmail.com>
2022-03-23 20:28:26 -0300

    Se comenzo a trabajar en el e-commerce

---
+++ frontend_mentor/ecommerce-product-page-main/js/e-commerce.js
@@ -0,0 +1,11 @@
+let sumaBtn = document.getElementById('suma');
+let restaBtn = document.getElementById('resta');
+let countProduct = document.getElementById('countPro');
+
+sumaBtn.onclick = () => {
+    
+}
+
+restaBtn.onclick = () => {
+    
+}
