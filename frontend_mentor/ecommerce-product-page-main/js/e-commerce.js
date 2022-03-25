b6866b82e6b34b5c9f5f544e7a014486caa58cfb
Omar Zavala <influencia.x.94@gmail.com>
2022-03-25 16:36:46 -0300

    se actualiza progreso

--- frontend_mentor/ecommerce-product-page-main/js/e-commerce.js
+++ frontend_mentor/ecommerce-product-page-main/js/e-commerce.js
@@ -4,21 +4,42 @@ let smallImg1 = document.getElementById('sm-1');
 let smallImg2 = document.getElementById('sm-2');
 let smallImg3 = document.getElementById('sm-3');
 let smallImg4 = document.getElementById('sm-4');
+//filtros div
+let filtro1 = document.getElementById('filtro-1');
+let filtro2 = document.getElementById('filtro-2');
+let filtro3 = document.getElementById('filtro-3');
+let filtro4 = document.getElementById('filtro-4'); 
 
 smallImg1.onclick = () => {
     bigImage.src = '/frontend_mentor/ecommerce-product-page-main/images/image-product-1.jpg';
+    filtro1.style.cssText = 'background-color: rgba(255, 255, 255, 0.336); border: 4px solid var(--Orange); width: 96px; height: 96px; border-radius: 16px;';
+    filtro2.style = null;
+    filtro3.style = null;
+    filtro4.style = null;
 }
 
 smallImg2.onclick = () => {
     bigImage.src = '/frontend_mentor/ecommerce-product-page-main/images/image-product-2.jpg';
+    filtro2.style.cssText = 'background-color: rgba(255, 255, 255, 0.336); border: 4px solid var(--Orange); width: 96px; height: 96px; border-radius: 16px;';
+    filtro1.style = null;
+    filtro3.style = null;
+    filtro4.style = null;
 }
 
 smallImg3.onclick = () => {
     bigImage.src = '/frontend_mentor/ecommerce-product-page-main/images/image-product-3.jpg';
+    filtro3.style.cssText = 'background-color: rgba(255, 255, 255, 0.336); border: 4px solid var(--Orange); width: 96px; height: 96px; border-radius: 16px;';
+    filtro1.style = null;
+    filtro2.style = null;
+    filtro4.style = null;
 }
 
 smallImg4.onclick = () => {
     bigImage.src = '/frontend_mentor/ecommerce-product-page-main/images/image-product-4.jpg';
+    filtro4.style.cssText = 'background-color: rgba(255, 255, 255, 0.336); border: 4px solid var(--Orange); width: 96px; height: 96px; border-radius: 16px;';
+    filtro1.style = null;
+    filtro2.style = null;
+    filtro3.style = null;
 }
 
     /* big image control */
