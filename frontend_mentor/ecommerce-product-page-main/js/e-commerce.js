15070a739876795fd1eef4736fe50e7958dde5b5
Omar Zavala <influencia.x.94@gmail.com>
2022-03-25 18:52:28 -0300

    Se agregan correcciones

--- frontend_mentor/ecommerce-product-page-main/js/e-commerce.js
+++ frontend_mentor/ecommerce-product-page-main/js/e-commerce.js
@@ -52,6 +52,8 @@ let sumaBtn = document.getElementById('suma');
 let restaBtn = document.getElementById('resta');
 let countProduct = document.getElementById('countPro');
 
+let btnAdd = document.getElementById('addCarts');
+
 sumaBtn.onclick = () => {
     countProduct.value = parseInt(countProduct.value, 0) + 1;
 }
@@ -60,5 +62,18 @@ restaBtn.onclick = () => {
     countProduct.value = parseInt(countProduct.value, 0) - 1;
     if (countProduct.value <= 0) {
         countProduct.value = 0
+    }
+}
+
+//btn add to carts
+let shopNumber = document.getElementById('shopNumber');
+
+btnAdd.onclick = () => {
+    if (countProduct.value > 0) {
+        shopNumber.style.cssText = 'background-color: var(--Orange); width: 24px; height: 19px; border-radius: 8px; position: absolute; top: -10px; left: 5px; font-size: 15px; text-align: center; color: white; padding: 0px 0px;';
+        shopNumber.innerHTML = countProduct.value;
+    } else {
+        shopNumber.style.cssText = null;
+        shopNumber.innerHTML = null;
     }
 }
