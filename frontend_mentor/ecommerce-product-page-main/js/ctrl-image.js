9a255ed044b9ec025d9eb1f5e4ad34555da810cd
Omar Zavala <influencia.x.94@gmail.com>
2022-03-27 14:58:12 -0300

    Se agrego popup de cart product shop

--- frontend_mentor/ecommerce-product-page-main/js/ctrl-image.js
+++ frontend_mentor/ecommerce-product-page-main/js/ctrl-image.js
@@ -52,40 +52,6 @@ smallImg4.onclick = () => {
     filtro3.style = null;
 }
 
-//
-// control count product
-//
-let sumaBtn = document.getElementById('suma');
-let restaBtn = document.getElementById('resta');
-let countProduct = document.getElementById('countPro');
-
-let btnAdd = document.getElementById('addCarts');
-
-sumaBtn.onclick = () => {
-    countProduct.value = parseInt(countProduct.value, 0) + 1;
-}
-
-restaBtn.onclick = () => {
-    countProduct.value = parseInt(countProduct.value, 0) - 1;
-    if (countProduct.value <= 0) {
-        countProduct.value = 0
-    }
-}
-
-//
-//btn add to carts
-//
-let shopNumber = document.getElementById('shopNumber');
-
-btnAdd.onclick = () => {
-    if (countProduct.value > 0) {
-        shopNumber.style.cssText = 'background-color: var(--Orange); width: 24px; height: 19px; border-radius: 8px; position: absolute; top: -10px; left: 5px; font-size: 15px; text-align: center; color: white; padding: 0px 0px;';
-        shopNumber.innerHTML = countProduct.value;
-    } else {
-        shopNumber.style.cssText = null;
-        shopNumber.innerHTML = null;
-    }
-}
 
 //
 //divs overlay
