e1021bbf0cba623a02f70a9b7e2d130f28f45a18
Omar Zavala <influencia.x.94@gmail.com>
2022-03-30 17:32:29 -0300

    Se actualiza diseño

--- frontend_mentor/ecommerce-product-page-main/js/ctrl-image.js
+++ frontend_mentor/ecommerce-product-page-main/js/ctrl-image.js
@@ -52,7 +52,6 @@ smallImg4.onclick = () => {
     filtro3.style = null;
 }
 
-
 //
 //divs overlay
 //
@@ -76,11 +75,14 @@ let filtro2Pop = document.getElementById('filtro-2-pop');
 let filtro3Pop = document.getElementById('filtro-3-pop');
 let filtro4Pop = document.getElementById('filtro-4-pop');
 
+
 //
 // big image control
 //
 bigImage.onclick = () => {
-    overlay.classList.add('active');
+    if (document.body.clientWidth > 768) {
+        overlay.classList.add('active');
+    }
 }
 
 closePop.onclick = () => {
@@ -119,4 +121,71 @@ smallImg4Pop.onclick = () => {
     filtro1Pop.style = null;
     filtro2Pop.style = null;
     filtro3Pop.style = null;
+}
+
+let prev = document.getElementById('prev-img');
+let next = document.getElementById('next-img');
+
+prev.onclick = () => {
+    try {
+        if (bigImagePop.src == "http://127.0.0.1:5500/frontend_mentor/ecommerce-product-page-main/images/image-product-1.jpg") {
+        bigImagePop.src = '/frontend_mentor/ecommerce-product-page-main/images/image-product-4.jpg';
+        filtro4Pop.style.cssText = 'background-color: rgba(255, 255, 255, 0.336); border: 4px solid var(--Orange); width: 96px; height: 96px; border-radius: 16px;';
+        filtro1Pop.style = null;
+        filtro2Pop.style = null;
+        filtro3Pop.style = null;
+    } else if(bigImagePop.src == "http://127.0.0.1:5500/frontend_mentor/ecommerce-product-page-main/images/image-product-4.jpg") {
+        bigImagePop.src = '/frontend_mentor/ecommerce-product-page-main/images/image-product-3.jpg';
+        filtro3Pop.style.cssText = 'background-color: rgba(255, 255, 255, 0.336); border: 4px solid var(--Orange); width: 96px; height: 96px; border-radius: 16px;';
+        filtro1Pop.style = null;
+        filtro2Pop.style = null;
+        filtro4Pop.style = null;
+    } else if(bigImagePop.src == "http://127.0.0.1:5500/frontend_mentor/ecommerce-product-page-main/images/image-product-3.jpg") {
+        bigImagePop.src = '/frontend_mentor/ecommerce-product-page-main/images/image-product-2.jpg';
+        filtro2Pop.style.cssText = 'background-color: rgba(255, 255, 255, 0.336); border: 4px solid var(--Orange); width: 96px; height: 96px; border-radius: 16px;';
+        filtro1Pop.style = null;
+        filtro3Pop.style = null;
+        filtro4Pop.style = null;
+    } else {
+        bigImagePop.src = '/frontend_mentor/ecommerce-product-page-main/images/image-product-1.jpg';
+        filtro1Pop.style.cssText = 'background-color: rgba(255, 255, 255, 0.336); border: 4px solid var(--Orange); width: 96px; height: 96px; border-radius: 16px;';
+        filtro2Pop.style = null;
+        filtro3Pop.style = null;
+        filtro4Pop.style = null;
+    }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+next.onclick = () => {
+    try {
+        if (bigImagePop.src == "http://127.0.0.1:5500/frontend_mentor/ecommerce-product-page-main/images/image-product-1.jpg") {
+        bigImagePop.src = '/frontend_mentor/ecommerce-product-page-main/images/image-product-2.jpg';
+        filtro2Pop.style.cssText = 'background-color: rgba(255, 255, 255, 0.336); border: 4px solid var(--Orange); width: 96px; height: 96px; border-radius: 16px;';
+        filtro1Pop.style = null;
+        filtro3Pop.style = null;
+        filtro4Pop.style = null;
+    } else if(bigImagePop.src == "http://127.0.0.1:5500/frontend_mentor/ecommerce-product-page-main/images/image-product-2.jpg") {
+        bigImagePop.src = '/frontend_mentor/ecommerce-product-page-main/images/image-product-3.jpg';
+        filtro3Pop.style.cssText = 'background-color: rgba(255, 255, 255, 0.336); border: 4px solid var(--Orange); width: 96px; height: 96px; border-radius: 16px;';
+        filtro1Pop.style = null;
+        filtro2Pop.style = null;
+        filtro4Pop.style = null;
+    } else if(bigImagePop.src == "http://127.0.0.1:5500/frontend_mentor/ecommerce-product-page-main/images/image-product-3.jpg") {
+        bigImagePop.src = '/frontend_mentor/ecommerce-product-page-main/images/image-product-4.jpg';
+        filtro4Pop.style.cssText = 'background-color: rgba(255, 255, 255, 0.336); border: 4px solid var(--Orange); width: 96px; height: 96px; border-radius: 16px;';
+        filtro1Pop.style = null;
+        filtro2Pop.style = null;
+        filtro3Pop.style = null;
+    } else {
+        bigImagePop.src = '/frontend_mentor/ecommerce-product-page-main/images/image-product-1.jpg';
+        filtro1Pop.style.cssText = 'background-color: rgba(255, 255, 255, 0.336); border: 4px solid var(--Orange); width: 96px; height: 96px; border-radius: 16px;';
+        filtro2Pop.style = null;
+        filtro3Pop.style = null;
+        filtro4Pop.style = null;
+    }
+    } catch (error) {
+        console.log(error);
+    }
 }
