ff4cde815aa18df5e4657f832b3531b92c2ab572
Omar Zavala <influencia.x.94@gmail.com>
2022-04-16 20:46:28 -0400

    Se guarda configuracion del proyecto

---
+++ js/works.js
@@ -0,0 +1,15 @@
+let calc = document.getElementsByClassName('overlay-work')[0];
+let calendar = document.getElementsByClassName('overlay-work')[1];
+let fornendM = document.getElementsByClassName('overlay-work')[2];
+
+calc.onclick = () => {
+    location.replace('https://github.com/Mangostar1');
+};
+
+calendar.onclick = () => {
+    location.replace('https://github.com/Mangostar1');
+};
+
+fornendM.onclick = () => {
+    location.replace('https://github.com/Mangostar1');
+};
