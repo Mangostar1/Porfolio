0d1645458256e654fb548643f355251697bcde25
Omar Zavala <influencia.x.94@gmail.com>
2022-04-19 00:03:41 -0400

    Se guarda progreso

--- send.php
+++ send.php
@@ -1,13 +1,15 @@
 <?php 
 
     $name = $_POST['name'];
+
     $subject = $_POST['subject'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-
-    $to = 'influencia.x.94@gmail.com'; //Esta variable almacena el correo al que llegara el mensaje
-    $headers = 'From: ' . $email . "\r\n"; //Esta variable almacena el correo del remitente
-
-    echo $name . ' ' . $subject . ' ' . $email . ' ' . $message;
-
+    $to = 'influencia.x.94@gmail.com';
+    
+    if (mail($to, $subject, $message)) {
+        echo 'mensaje enviado';
+    } else {
+        echo 'Error, mensaje no enviado';
+    }
 ?>
