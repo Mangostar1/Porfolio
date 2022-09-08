edd1278d0755351f454691981a944dd292dd600e
Omar <93488662+Mangostar1@users.noreply.github.com>
2022-09-07 21:30:10 -0300

    Se arregla bug en html que afectava al slider y se refactoriza el slider

--- js/slider.js
+++ js/slider.js
@@ -19,94 +19,87 @@ box2.style.display = 'none';
 box3.style.display = 'none';
 dot1.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
 
-prev1.onclick = () => {
-    box1.style.display = 'none';
-    box2.style.display = 'none';
-    box3.style.display = 'block';
+document.addEventListener('click', (e) => {
+    console.log(e.target);
+    if (e.target.matches('#prev1')) {
+        box1.style.display = 'none';
+        box2.style.display = 'none';
+        box3.style.display = 'block';
 
-    dot1.style.cssText = 'background-color: grey; transform: scale(1);';
-    dot2.style.cssText = 'background-color: grey; transform: scale(1);';
-    dot3.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
-}
-
-next1.onclick = () => {
-    box1.style.display = 'none';
-    box3.style.display = 'none';
-    box2.style.display = 'block';
-
-    dot1.style.cssText = 'background-color: grey; transform: scale(1);';
-    dot2.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
-    dot3.style.cssText = 'background-color: grey; transform: scale(1);';
-}
-
-prev2.onclick = () => {
-    box2.style.display = 'none';
-    box3.style.display = 'none';
-    box1.style.display = 'block';
-
-    dot1.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
-    dot2.style.cssText = 'background-color: grey; transform: scale(1);';
-    dot3.style.cssText = 'background-color: grey; transform: scale(1);';
-}
-
-next2.onclick = () => {
-    box1.style.display = 'none';
-    box2.style.display = 'none';
-    box3.style.display = 'block';
-
-    dot1.style.cssText = 'background-color: grey; transform: scale(1);';
-    dot2.style.cssText = 'background-color: grey; transform: scale(1);';
-    dot3.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
-}
-
-prev3.onclick = () => {
-    box1.style.display = 'none';
-    box3.style.display = 'none';
-    box2.style.display = 'block';
-
-    dot1.style.cssText = 'background-color: grey; transform: scale(1);';
-    dot2.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
-    dot3.style.cssText = 'background-color: grey; transform: scale(1);';
-}
-
-next3.onclick = () => {
-    box2.style.display = 'none';
-    box3.style.display = 'none';
-    box1.style.display = 'block';
-
-    dot1.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
-    dot2.style.cssText = 'background-color: grey; transform: scale(1);';
-    dot3.style.cssText = 'background-color: grey; transform: scale(1);';
-}
-
-//dots
-
-dot1.onclick = () =>{
-    box2.style.display = 'none';
-    box3.style.display = 'none';
-    box1.style.display = 'block';
-
-    dot1.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
-    dot2.style.cssText = 'background-color: grey; transform: scale(1);';
-    dot3.style.cssText = 'background-color: grey; transform: scale(1);';
-}
-
-dot2.onclick = () =>{
-    box1.style.display = 'none';
-    box3.style.display = 'none';
-    box2.style.display = 'block';
-
-    dot1.style.cssText = 'background-color: grey; transform: scale(1);';
-    dot2.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
-    dot3.style.cssText = 'background-color: grey; transform: scale(1);';
-}
-
-dot3.onclick = () =>{
-    box1.style.display = 'none';
-    box2.style.display = 'none';
-    box3.style.display = 'block';
-
-    dot1.style.cssText = 'background-color: grey; transform: scale(1);';
-    dot2.style.cssText = 'background-color: grey; transform: scale(1);';
-    dot3.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
-}
+        dot1.style.cssText = 'background-color: grey; transform: scale(1);';
+        dot2.style.cssText = 'background-color: grey; transform: scale(1);';
+        dot3.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
+    }
+    if (e.target.matches('#next1')) {
+        box1.style.display = 'none';
+        box2.style.display = 'block';
+        box3.style.display = 'none';
+    
+        dot1.style.cssText = 'background-color: grey; transform: scale(1);';
+        dot2.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
+        dot3.style.cssText = 'background-color: grey; transform: scale(1);';
+    }
+    if (e.target.matches('#prev2')) {
+        box1.style.display = 'block';
+        box2.style.display = 'none';
+        box3.style.display = 'none';
+    
+        dot1.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
+        dot2.style.cssText = 'background-color: grey; transform: scale(1);';
+        dot3.style.cssText = 'background-color: grey; transform: scale(1);';
+    }
+    if (e.target.matches('#next2')) {
+        box1.style.display = 'none';
+        box2.style.display = 'none';
+        box3.style.display = 'block';
+    
+        dot1.style.cssText = 'background-color: grey; transform: scale(1);';
+        dot2.style.cssText = 'background-color: grey; transform: scale(1);';
+        dot3.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
+    }
+    if (e.target.matches('#prev3')) {
+        box1.style.display = 'none';
+        box2.style.display = 'block';
+        box3.style.display = 'none';
+    
+        dot1.style.cssText = 'background-color: grey; transform: scale(1);';
+        dot2.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
+        dot3.style.cssText = 'background-color: grey; transform: scale(1);';
+    }
+    if (e.target.matches('#next3')) {
+        box1.style.display = 'block';
+        box2.style.display = 'none';
+        box3.style.display = 'none';
+    
+        dot1.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
+        dot2.style.cssText = 'background-color: grey; transform: scale(1);';
+        dot3.style.cssText = 'background-color: grey; transform: scale(1);';
+    }
+    if (e.target.matches('#dot1')) {
+        box2.style.display = 'none';
+        box3.style.display = 'none';
+        box1.style.display = 'block';
+    
+        dot1.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
+        dot2.style.cssText = 'background-color: grey; transform: scale(1);';
+        dot3.style.cssText = 'background-color: grey; transform: scale(1);';
+    }
+    if (e.target.matches('#dot2')) {
+        box1.style.display = 'none';
+        box3.style.display = 'none';
+        box2.style.display = 'block';
+    
+        dot1.style.cssText = 'background-color: grey; transform: scale(1);';
+        dot2.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
+        dot3.style.cssText = 'background-color: grey; transform: scale(1);';
+    }
+    if (e.target.matches('#dot3')) {
+        box1.style.display = 'none';
+        box2.style.display = 'none';
+        box3.style.display = 'block';
+    
+        dot1.style.cssText = 'background-color: grey; transform: scale(1);';
+        dot2.style.cssText = 'background-color: grey; transform: scale(1);';
+        dot3.style.cssText = 'background-color: #2743F5; transform: scale(1.2);';
+    }
+});
