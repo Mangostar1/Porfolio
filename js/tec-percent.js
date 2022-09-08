b932fc88a6a83013369d10e37a52316fc213a49a
Omar <93488662+Mangostar1@users.noreply.github.com>
2022-09-08 16:28:01 -0300

    Se agregan las tecnologias que manejo y se corrigen las anteriores

--- js/tec-percent.js
+++ js/tec-percent.js
@@ -9,26 +9,30 @@ percents[0].style.cssText = `width: ${html}`;
 let css = p[1].innerHTML;
 percents[1].style.cssText = `width: ${css}`;
 
+//Sass
+let sass = p[2].innerHTML;
+percents[2].style.cssText = `width: ${sass}`;
+
 //Javascript
-let JavaScript = p[2].innerHTML;
-percents[2].style.cssText = `width: ${JavaScript}`;
+let javascript = p[3].innerHTML;
+percents[3].style.cssText = `width: ${javascript}`;
 
-//git
-let Git = p[3].innerHTML;
-percents[3].style.cssText = `width: ${Git}`;
-
-//NPM
-let NPM = p[4].innerHTML;
-percents[4].style.cssText = `width: ${NPM}`;
-
-//Node
-let Node = p[5].innerHTML;
-percents[5].style.cssText = `width: ${Node}`;
+//Git
+let git = p[4].innerHTML;
+percents[4].style.cssText = `width: ${git}`;
 
 //React
-let React = p[6].innerHTML;
-percents[6].style.cssText = `width: ${React}`;
+let react = p[5].innerHTML;
+percents[5].style.cssText = `width: ${react}`;
 
-//Bootstrap
-let Bootstrap = p[7].innerHTML;
-percents[7].style.cssText = `width: ${Bootstrap}`;
+//Tailwind
+let Tailwind = p[6].innerHTML;
+percents[6].style.cssText = `width: ${Tailwind}`;
+
+//NPM
+let npm = p[7].innerHTML;
+percents[7].style.cssText = `width: ${npm}`;
+
+//Node
+let node = p[8].innerHTML;
+percents[8].style.cssText = `width: ${node}`;
