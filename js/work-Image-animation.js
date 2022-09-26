4db1a2dd5911d844040794da318752c97c692d94
Omar <93488662+Mangostar1@users.noreply.github.com>
2022-09-26 20:05:42 -0300

    Se arregla bug en animaciones

--- js/work-Image-animation.js
+++ js/work-Image-animation.js
@@ -4,29 +4,30 @@ const descriptWork= document.getElementsByClassName('descript-work');
 
 const imgWorks = document.querySelectorAll('.img-works');
 
-const timeTrans = .4;
+const timeTrans = .2;
 const scale = 1.2;
 
 window.addEventListener('mouseover', (e) => {
-    if (e.target === overlayWork[0] || e.target === tecUsed[0] || e.target === descriptWork[0]) {
+    console.log(e.target);
+    if (e.target === overlayWork[0] || e.target === tecUsed[0] || e.target === tecUsed[1] || e.target === descriptWork[0]) {
             imgWorks[0].style.cssText = `transition: all ${timeTrans}s ease; transform: scale(${scale})`;
     } 
-    else if (e.target === overlayWork[1] || e.target === tecUsed[1] || e.target === descriptWork[1]) {
+    else if (e.target === overlayWork[1] || e.target === tecUsed[2] || e.target === tecUsed[3] || e.target === descriptWork[1]) {
         imgWorks[1].style.cssText = `transition: all ${timeTrans}s ease; transform: scale(${scale})`;
     } 
-    else if (e.target === overlayWork[2] || e.target === tecUsed[2] || e.target === descriptWork[2]) {
+    else if (e.target === overlayWork[2] || e.target === tecUsed[4] || e.target === tecUsed[5] || e.target === descriptWork[2]) {
         imgWorks[2].style.cssText = `transition: all ${timeTrans}s ease; transform: scale(${scale})`;
     } 
-    else if (e.target === overlayWork[3] || e.target === tecUsed[3] || e.target === descriptWork[3]) {
+    else if (e.target === overlayWork[3] || e.target === tecUsed[6] || e.target === tecUsed[7] || e.target === descriptWork[3]) {
         imgWorks[3].style.cssText = `transition: all ${timeTrans}s ease; transform: scale(${scale})`;
     } 
-    else if (e.target === overlayWork[4] || e.target === tecUsed[4] || e.target === descriptWork[4]) {
+    else if (e.target === overlayWork[4] || e.target === tecUsed[8] || e.target === tecUsed[9] || e.target === descriptWork[4]) {
         imgWorks[4].style.cssText = `transition: all ${timeTrans}s ease; transform: scale(${scale})`;
     } 
-    else if (e.target === overlayWork[5] || e.target === tecUsed[5] || e.target === descriptWork[5]) {
+    else if (e.target === overlayWork[5] || e.target === tecUsed[10] || e.target === tecUsed[11] || e.target === descriptWork[5]) {
         imgWorks[5].style.cssText = `transition: all ${timeTrans}s ease; transform: scale(${scale}`;
     } 
-    else if (e.target === overlayWork[6] || e.target === tecUsed[6] || e.target === descriptWork[6]) {
+    else if (e.target === overlayWork[6] || e.target === tecUsed[12] || e.target === tecUsed[13] || e.target === descriptWork[6]) {
         imgWorks[6].style.cssText = `transition: all ${timeTrans}s ease; transform: scale(${scale}`;
     } 
     else {
