ca094f49422503d6a4a6337d302fbbdc5617cf48
Omar <93488662+Mangostar1@users.noreply.github.com>
2022-09-27 17:43:19 -0300

    Se agregan variables

--- js/tec-percent.js
+++ js/tec-percent.js
@@ -1,34 +1,36 @@
-let percents = document.getElementsByClassName('percent-tec'); //divs with class percent-tec
-let p = document.getElementsByClassName('percent-p'); //divs with class percent-p
+let percents = document.getElementsByClassName('percent-tec');
+let p = document.getElementsByClassName('percent-p');
+
+let imgHTML = document.getElementById('img-html');
+let imgCSS = document.getElementById('img-css');
+let imgSASS = document.getElementById('img-sass');
+let imgJS = document.getElementById('img-js');
+let imgGIT = document.getElementById('img-git');
+let imgREACT = document.getElementById('img-react');
+let imgTAILWIND = document.getElementById('img-tailwind');
 
 /*---------------------------------*/
 /* Default width without animation */
 /*---------------------------------*/
-//HTML5
+
 let html = p[0].innerHTML;
 percents[0].style.cssText = `width: ${html}`;
 
-//CSS 3
 let css = p[1].innerHTML;
 percents[1].style.cssText = `width: ${css}`;
 
-//Sass
 let sass = p[2].innerHTML;
 percents[2].style.cssText = `width: ${sass}`;
 
-//Javascript
 let javascript = p[3].innerHTML;
 percents[3].style.cssText = `width: ${javascript}`;
 
-//Git
 let git = p[4].innerHTML;
 percents[4].style.cssText = `width: ${git}`;
 
-//React
 let react = p[5].innerHTML;
 percents[5].style.cssText = `width: ${react}`;
 
-//Tailwind
 let Tailwind = p[6].innerHTML;
 percents[6].style.cssText = `width: ${Tailwind}`;
 
