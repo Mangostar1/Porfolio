d9612b6af5f98e7f4cca509bbd378d09b1183bf9
Omar Zavala <influencia.x.94@gmail.com>
2022-12-28 10:49:14 -0300

    Se realizan cambios

--- js/Home.js
+++ js/Home.js
@@ -8,20 +8,6 @@ export function Home(element) {
             <p class="card-text">
                 Frontend Developer
             </p>
-            <div class="social-media-contaienr">
-                <a class="social-media-link" href="https://www.instagram.com/omar.zavala0/" title="instagram">
-                    <img class="social-media-icons" src="./icons/icons8-instagram.svg" alt="">
-                </a>
-                <a class="social-media-link" href="https://github.com/Mangostar1" title="github">
-                    <img class="social-media-icons github-social-media" src="./icons/github.svg" alt="">
-                </a>
-                <a class="social-media-link" href="https://www.linkedin.com/in/omar-zavala-ugarte/" title="linkedin">
-                    <img class="social-media-icons" src="./icons/LI-In-Bug.png" alt="">
-                </a>
-                <a class="social-media-link" href="mailto:influencia.x.94@gmail.com" title="gmail">
-                    <img class="social-media-icons" src="./icons/gmail-color.png" alt="">
-                </a>
-            </div>
         </article>`;
 
     element.appendChild(homeContent);
