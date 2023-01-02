3798574e84cf750b2d72744983a7e715ad72b5ff
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-01-02 20:42:28 -0300

    Se guardan cambios hasta ahora

--- js/Home.js
+++ js/Home.js
@@ -4,6 +4,12 @@ export function Home(element) {
     homeContent.id = "sec-welcom-id"
     homeContent.innerHTML = 
         `<article class="article" id="welcome">
+            <div class="hello-content" id="content-hello">
+                <p class="hello-words" id="p-hello">HOLA</p>
+            </div>
+            <div class="hello-content" id="content-world">
+                <p class="hello-words" id="p-world">MUNDO</p>
+            </div>
             <p class="card-text">
                 Frontend Developer
             </p>
