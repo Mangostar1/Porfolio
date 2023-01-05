e166ff0297b0b25ee718637203cd99cbed81c24b
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-01-05 14:19:21 -0300

    Se elimina codigo sass innecesario

--- js/Work.js
+++ js/Work.js
@@ -42,18 +42,6 @@ export function Work(element) {
         else if (e.target === overlayWork[2] || e.target === tecUsed[4] || e.target === tecUsed[5] || e.target === descriptWork[2]) {
             Animations(2);
         } 
-        else if (e.target === overlayWork[3] || e.target === tecUsed[6] || e.target === tecUsed[7] || e.target === descriptWork[3]) {
-            Animations(3);
-        } 
-        else if (e.target === overlayWork[4] || e.target === tecUsed[8] || e.target === tecUsed[9] || e.target === descriptWork[4]) {
-            Animations(4);
-        } 
-        else if (e.target === overlayWork[5] || e.target === tecUsed[10] || e.target === tecUsed[11] || e.target === descriptWork[5]) {
-            Animations(5);
-        } 
-        else if (e.target === overlayWork[6] || e.target === tecUsed[12] || e.target === tecUsed[13] || e.target === descriptWork[6]) {
-            Animations(6);
-        } 
         else {
             for (let i = 0; i < overlayWork.length; i++) {
                 imgWorks[i].style.cssText = `transition: all ${timeTrans}s ease; transform: scale(1)`;
