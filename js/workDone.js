8ece0d7e9bb3f9c60c9627069725ce9c8eee1f15
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-01-05 14:14:19 -0300

    Se corrige forma de mandar el html desde js

--- js/workDone.js
+++ js/workDone.js
@@ -1,1 +1,60 @@
-const calendar = ``;
+export const calendar = `
+    <div class="content-works-1">
+        <div class="content-works-1 content-works-2">
+            <div class="content-img-works">
+                <div class="img-works" id="img-calendar"></div>
+            </div>
+            <div class="overlay-work">
+                <p class="texto-work">
+                    Calendario Web
+                </p>
+                <p class="descript-work">
+                    Calendario web creado para <a class="otros-links" href="https://rainmakers.cl/" rel="noopener">rainmakers.cl</a>. Esta escrito únicamente 
+                    con javascript vanilla con la finalidad de brindar mayor personalización al proyecto.
+                </p>
+                <a href="https://mangostar1.github.io/Calendar/" class="tec-used" target="_blank" rel="noopener">Website</a>
+                <a href="https://github.com/Mangostar1/Calendar" class="tec-used" target="_blank" rel="noopener">Repositorio</a>
+            </div>
+        </div>
+    </div>
+`;
+
+export const ejercicios = `
+    <div class="content-works-1">
+        <div class="content-works-2">
+            <div class="content-img-works">
+                <div class="img-works" id="img-calc"></div>
+            </div>
+            <div class="overlay-work">
+                <p class="texto-work">
+                    Ejercicios
+                </p>
+                <p class="descript-work">
+                    En esta página se encuentran todos los pequeños proyectos personales que he realizado a modo de ejercicio.
+                </p>
+                <a href="https://ejercicios-web.netlify.app/" class="tec-used" target="_blank" rel="noopener">Website</a>
+                <a href="https://github.com/Mangostar1/Mis-trabjos-peque-os" class="tec-used" target="_blank" rel="noopener">Repositorio</a>
+            </div>
+        </div>
+    </div>
+`;
+
+export const frontendRetos = `
+    <div class="content-works-1">
+        <div class="content-works-2">
+            <div class="content-img-works">
+                <div class="img-works" id="img-ipAddress"></div>
+            </div>
+            <div class="overlay-work">
+                <p class="texto-work">
+                    Retos de Frontend Mentor
+                </p>
+                <p class="descript-work">
+                    En esta pagina estan alojados todos los retos que he realizado de la pagina <a class="otros-links" href="https://www.frontendmentor.io/" rel="noopener">Frontend Mentor</a>
+                </p>
+                <a href="https://mangostar1.github.io/Retos_Frontend/ip-address-tracker-master/ip-index.html" class="tec-used" target="_blank" rel="noopener">Website</a>
+                <a href="https://github.com/Mangostar1/Frontend_mentor/tree/main/ip-address-tracker-master" class="tec-used" target="_blank" rel="noopener">Repositorio</a>
+            </div>
+        </div>
+    </div>
+`;
