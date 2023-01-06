d821e056ced5947f9964e76dfdd63df6123ced39
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-01-06 17:40:56 -0300

    Se actualiza el diseño del about, se continuara trabajando en el

--- js/technologies.js
+++ js/technologies.js
@@ -1,83 +1,54 @@
 export const htmlTec = 
-    `<div class="tec-div">
-        <a class="links-tec" href="https://developer.mozilla.org/es/docs/Glossary/HTML5">
-            <p>HTML 5</p>
-            <img src="./icons/logos_tecnologias/html5-svgrepo-com.svg" alt="html" id="img-html">
-        </a>
-    </div>`;
+    `<li class="list-tec">
+        HTML 5
+    </li>`;
 
 export const cssTec = 
-    `<div class="tec-div">
-        <a class="links-tec" href="https://developer.mozilla.org/es/docs/Web/CSS">
-            <p>CSS 3</p>
-            <img src="./icons/logos_tecnologias/css3-svgrepo-com.svg" alt="css" id="img-css">
-        </a>
-    </div>`;
+    `<li class="list-tec">
+        CSS 3
+    </li>`;
 
 export const sassTec = 
-    `<div class="tec-div">
-        <a class="links-tec" href="https://sass-lang.com/">
-            <p>SASS</p>
-            <img src="./icons/logos_tecnologias/sass-color.png" alt="sass" id="img-sass">
-        </a>
-    </div>`;
+    `<li class="list-tec">
+        SASS
+    </li>`;
 
 export const jsTec = 
-    `<div class="tec-div">
-        <a class="links-tec" href="https://developer.mozilla.org/es/docs/Web/JavaScript">
-            <P>Javascript</P>
-            <img src="./icons/logos_tecnologias/javascript-svgrepo-com.svg" alt="js" id="img-js">
-        </a>
-    </div>`;
+    `<li class="list-tec">
+        Javascript
+    </li>`;
 
 export const gitTec = 
-    `<div class="tec-div">
-        <a class="links-tec" href="https://git-scm.com/">
-            <P id="git-p">Git</P>
-            <img src="./icons/logos_tecnologias/git-svgrepo-com.svg" alt="GIT" id="img-git">
-        </a>
-    </div>`;
+    `<li class="list-tec">
+        Git
+    </li>`;
 
 export const reactTec = 
-    `<div class="tec-div">
-        <a class="links-tec" href="https://reactjs.org/">
-            <p>React JS</p>
-            <img src="./icons/logos_tecnologias/react-svgrepo-com.svg" alt="react" id="img-react">
-        </a>
-    </div>`;
+    `<li class="list-tec">
+        React JS
+    </li>`;
 
 export const tailwindTec = 
-    `<div class="tec-div">
-        <a class="links-tec" href="https://tailwindcss.com/">
-            <p>Tailwind</p>
-            <img src="./icons/logos_tecnologias/tailwindcss-color.svg" alt="tailwind" id="img-tailwind">
-        </a>
-    </div>`;
+    `<li class="list-tec">
+        Tailwind
+    </li>`;
 
 export const mysqlTec = 
-    `<div class="tec-div">
-        <a class="links-tec" href="https://www.mysql.com/">
-            <img src="./icons/logos_tecnologias/mysql.svg" alt="mysql" id="img-mysql">
-        </a>
-    </div>`;
+    `<li class="list-tec">
+        MySQL
+    </li>`;
 
 export const dockerTec = 
-    `<div class="tec-div">
-        <a class="links-tec" href="https://www.docker.com/">
-            <img src="./icons/logos_tecnologias/vertical-logo-monochromatic.webp" alt="Docker" id="img-docker">
-        </a>
-    </div>`;
+    `<li class="list-tec">
+        Docker
+    </li>`;
 
 export const nodeTec = 
-    `<div class="tec-div">
-        <a class="links-tec" href="https://nodejs.dev/">
-            <img src="./icons/logos_tecnologias/Node.js_logo.svg" alt="Node JS" id="img-node">
-        </a>
-    </div>`;
+    `<li class="list-tec">
+        NodeJS
+    </li>`;
 
 export const expressTec = 
-    `<div class="tec-div">
-        <a class="links-tec" href="https://expressjs.com/">
-            <img src="./icons/logos_tecnologias/expressjs.svg" alt="Express js" id="img-express">
-        </a>
-    </div>`;
+    `<li class="list-tec">
+        ExpressJS
+    </li>`;
