019a3e5140896bdf5a747ab44e13f647c2b11594
Omar <93488662+Mangostar1@users.noreply.github.com>
2023-02-21 09:53:04 -0300

    se termina de dar estilos y animaciones al hola mundo del Home de la pagina

--- js/Home.js
+++ js/Home.js
@@ -6,9 +6,11 @@ export function Home(element) {
         `<article class="article" id="welcome">
             <div class="hello-content" id="content-hello">
                 <p class="hello-words" id="p-hello">HOLA</p>
+                <p class="hello-words" id="p-omar">OMAR</p>
             </div>
             <div class="hello-content" id="content-world">
                 <p class="hello-words" id="p-world">MUNDO</p>
+                <p class="hello-words" id="p-zavala">ZAVALA</p>
             </div>
             <p class="card-text">
                 Frontend Developer
